4bc51c0ec51d9e90fb20b21b96cca4d894266df0
Abhishek Upperwal <a.upperwal@gmail.com>
2019-01-14 01:45:20 +0530

    per packet test for hole punching

---
+++ example/packet-test/packet.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	port := flag.String("p", "0", "listener port")
+
+	c, err := net.ListenPacket("udp4", "0.0.0.0:"+*port)
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter dest Multiaddr: ")
+	text, _ := reader.ReadString('\n')
+	text = strings.Trim(text, "\n")
+
+	addr, err := net.ResolveUDPAddr("udp4", text)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		_, err := c.WriteTo([]byte("hello"), addr)
+		if err != nil {
+			panic(err)
+		}
+		reader.ReadString('\n')
+	}
+}
