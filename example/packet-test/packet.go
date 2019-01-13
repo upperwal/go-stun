5da15e69fb73915d3bc82dc03d12c5e241e6c3d6
Abhishek Upperwal <a.upperwal@gmail.com>
2019-01-14 01:57:36 +0530

    packet sender

--- example/packet-test/packet.go
+++ example/packet-test/packet.go
@@ -22,17 +22,32 @@ func main() {
 	fmt.Print("Enter dest Multiaddr: ")
 	text, _ := reader.ReadString('\n')
 	text = strings.Trim(text, "\n")
+	ups := strings.Split(text, ",")
 
-	addr, err := net.ResolveUDPAddr("udp4", text)
+	addr1, err := net.ResolveUDPAddr("udp4", ups[0])
+	if err != nil {
+		panic(err)
+	}
+
+	addr2, err := net.ResolveUDPAddr("udp4", ups[1])
 	if err != nil {
 		panic(err)
 	}
 
 	for {
+		text, _ := reader.ReadString('\n')
+		text = strings.Trim(text, "\n")
+
+		var addr net.Addr
+		if text == "1" {
+			addr = addr1
+		} else {
+			addr = addr2
+		}
 		_, err := c.WriteTo([]byte("hello"), addr)
 		if err != nil {
 			panic(err)
 		}
-		reader.ReadString('\n')
+
 	}
 }
