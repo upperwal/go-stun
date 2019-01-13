87320aa5a8397a1bd0854fc27c9c3a383f0865f3
Abhishek Upperwal <a.upperwal@gmail.com>
2019-01-14 01:22:27 +0530

    more delay

--- client.go
+++ client.go
@@ -158,7 +158,7 @@ func (c *Client) bombardPackets(peer []byte) {
 			log.Error(err)
 			return
 		}
-		time.Sleep(time.Millisecond * 10)
+		time.Sleep(time.Millisecond * 100)
 	}
 
 	c.completionMapChan[maAddr.String()] <- true
